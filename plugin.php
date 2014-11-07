25613252d9db4005ed917fff3054550e565bf410
Fabio Heer <fabio.heer@ilub.unibe.ch>
2014-11-07 17:46:40 +0100

    Add ILIAS 4.3 compatibility

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'ctrlmm';
 $version = '2.4.00';
-$ilias_min_version = '4.4.000';
+$ilias_min_version = '4.3.000';
 $ilias_max_version = '4.5.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
