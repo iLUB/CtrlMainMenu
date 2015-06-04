5acc3cbf9860c85c9221804d41f9f66983aed6f0
Tomasz <thomas.kolonko@ilub.unibe.ch>
2015-06-04 13:18:33 +0200

    Version adapted

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'ctrlmm';
-$version = '2.4.00';
-$ilias_min_version = '4.3.000';
-$ilias_max_version = '4.5.999';
+$version = '3.1.00';
+$ilias_min_version = '4.4.000';
+$ilias_max_version = '5.1.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>
