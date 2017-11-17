0feecdb894b4dd82ae297a4baf1e1128b3cec840
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2017-11-17 14:51:01 +0100

    increased $ilias_max_version to 5.3.999

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'ctrlmm';
 $version = '3.3.05';
 $ilias_min_version = '5.0.000';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.3.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
